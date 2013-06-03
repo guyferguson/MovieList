79b89b4634015a166f86d45a1b8d412502c955e2
guyferguson <guyferguson@tpg.com.au>
2013-06-03 22:20:32 +1000

    Licked writing xml to textbox

--- MovieList/Form1.vb
+++ MovieList/Form1.vb
@@ -1,5 +1,7 @@
 ﻿
 Imports System.Xml
+Imports System.Text
+
 Public Class Form1
 
     Private Sub btSearch_Click(sender As Object, e As EventArgs) Handles btSearch.Click
@@ -8,8 +10,9 @@ Public Class Form1
         Dim webClient As New System.Net.WebClient
         Dim result As String = webClient.DownloadString("http://www.imdb.com/xml/find?xml=1&nr=1&tt=on&q=" & strMovieName)
         Dim mv As New Movie
+        result = result
         tbResult.Text = mv.showPossibles(result)
     End Sub
 
-  
+
 End Class
--- MovieList/movie.vb
+++ MovieList/movie.vb
@@ -6,34 +6,53 @@ Public Class Movie
 
     Private resultSet
 
-    'from a supplied string of xsml text, return a list of movie titles
+    'from a supplied string of xsml text, return a list of movie titles 
     '
     ' PRE: Valid XML string
     ' POST: 
 
     Public Function showPossibles(xmltext)
 
-        Dim output As StringBuilder = New StringBuilder()
-        
-        Using reader As XmlReader = XmlReader.Create(New StringReader(xmltext))
-            While reader.Read()
-                If reader.IsEmptyElement Then
-                    output.AppendLine(reader.Value & " is an exmply element - how sad" & vbCrLf)
-                End If
+        Dim myMovies As XElement = XElement.Parse(xmltext)
 
-                reader.ReadToFollowing("Description")
+        Dim titles = <t><%= From title In myMovies...<ImdbEntity>
+                             Select title.Elements.First.Value
+                        %></t>
 
-                Dim genre As String = reader.Value
-                output.AppendLine("The genre value: " + genre)
 
-                '  reader.ReadToFollowing("Description")
-                ' output.AppendLine("Content of the title element: " + reader.ReadElementContentAsString())
-            End While
 
-        End Using
+        Dim temp As StringBuilder = New StringBuilder(titles.ToString)
 
-        Return output.ToString()
 
+
+
+        Return temp.ToString
+
+        ''   Dim output As StringBuilder = New StringBuilder()
+
+        'Using reader As XmlReader = XmlReader.Create(New StringReader(xmltext))
+        '    While reader.Read()
+        '        If reader.IsEmptyElement Then
+        '            output.AppendLine(reader.Value & " is an exmply element - how sad" & vbCrLf)
+        '        End If
+
+        '        reader.ReadToFollowing("Description")
+
+        '        Dim genre As String = reader.Value
+        '        output.AppendLine("The genre value: " + genre)
+
+        '        '  reader.ReadToFollowing("Description")
+        '        ' output.AppendLine("Content of the title element: " + reader.ReadElementContentAsString())
+        '    End While
+
+        'End Using
+
+        'Return output.ToString()
+
+    End Function
+
+    Private Function phoneTypes() As Object
+        Throw New NotImplementedException
     End Function
 
 End Class
